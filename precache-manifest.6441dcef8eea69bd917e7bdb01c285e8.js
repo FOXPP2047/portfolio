842cfc2fa97df2422142c7e6db0eb70c90885e78
FOXPP2047 <qlxhdl55@gmail.com>
2020-05-29 02:02:50 -0700

    Updates

---
+++ precache-manifest.6441dcef8eea69bd917e7bdb01c285e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec851c358dd9b084f1c593939556b5e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06ef49dc4873cf26a670",
+    "url": "/portfolio/static/css/main.7d5d447a.chunk.css"
+  },
+  {
+    "revision": "d8ca229ea57ba44fad90",
+    "url": "/portfolio/static/js/2.86c33259.chunk.js"
+  },
+  {
+    "revision": "e62cd726ace41e13f340685b359392b1",
+    "url": "/portfolio/static/js/2.86c33259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ef49dc4873cf26a670",
+    "url": "/portfolio/static/js/main.fe224110.chunk.js"
+  },
+  {
+    "revision": "039eb055909a0ac81ca5",
+    "url": "/portfolio/static/js/runtime-main.3e7fea94.js"
+  },
+  {
+    "revision": "9f03a125c2a59d885ecdf7d1fbc2ded4",
+    "url": "/portfolio/static/media/bouncingmemory.9f03a125.png"
+  },
+  {
+    "revision": "0ea153da57e3ddcf529fa40a65ba0603",
+    "url": "/portfolio/static/media/lunokhod.0ea153da.png"
+  },
+  {
+    "revision": "d67b84d72417a654a520e2b7cdb16967",
+    "url": "/portfolio/static/media/profile.d67b84d7.jpg"
+  },
+  {
+    "revision": "6ca0d8d7b67535b14acc2853f1795516",
+    "url": "/portfolio/static/media/resume.6ca0d8d7.pdf"
+  }
+]);
