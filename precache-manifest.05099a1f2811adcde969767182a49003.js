1476e5c119ee811ec5011dcab780289760e957ad
Yongmin Cho <qlxhdl55@gmail.com>
2020-05-29 19:05:38 -0700

    Updates

---
+++ precache-manifest.05099a1f2811adcde969767182a49003.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec73adcb1394e48c78aeb4042412aa7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aef07e89887d9ce855f",
+    "url": "/static/css/main.99f73a4a.chunk.css"
+  },
+  {
+    "revision": "5db4e7b198323173ae35",
+    "url": "/static/js/2.c6d40c7d.chunk.js"
+  },
+  {
+    "revision": "a2dc516119136b9086f12ba7a2d22946",
+    "url": "/static/js/2.c6d40c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aef07e89887d9ce855f",
+    "url": "/static/js/main.974b2e8b.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  },
+  {
+    "revision": "9f03a125c2a59d885ecdf7d1fbc2ded4",
+    "url": "/static/media/bouncingmemory.9f03a125.png"
+  },
+  {
+    "revision": "0ea153da57e3ddcf529fa40a65ba0603",
+    "url": "/static/media/lunokhod.0ea153da.png"
+  },
+  {
+    "revision": "d67b84d72417a654a520e2b7cdb16967",
+    "url": "/static/media/profile.d67b84d7.jpg"
+  },
+  {
+    "revision": "412ec152803fc41806340fd379b13bf7",
+    "url": "/static/media/resume.412ec152.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7eec60bb637d19d1c1a5dfc35498ef1f.js"
+  "/precache-manifest.05099a1f2811adcde969767182a49003.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
