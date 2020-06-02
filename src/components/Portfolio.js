1acd75addccba8a127f5e79238b753bcb9d44dca
Yongmin Cho <qlxhdl55@gmail.com>
2020-06-02 00:54:54 -0700

    add riot discord project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,22 +11,29 @@ import {
     Typography,
     CardContent,
 } from "@material-ui/core"
+import Particles from "react-particles-js";
 import Navbar from "./Navbar";
+
 import project1 from "../img/projects/bouncingmemory.png";
 import project2 from "../img/projects/lunokhod.png";
 import project3 from "../img/projects/welcometothefuture.png";
+import project4 from "../img/projects/discord.jpg";
 
-import { bmExplanation, mdExplanation, wfExplanation } from "./Explanation";
+import { bmExplanation, mdExplanation, wfExplanation, riotExplanation } from "./Explanation";
 
 const useStyles = makeStyles({
     mainContainer: {
-        background: "#303960",
+        //background: "#303960",
         height: "100%",
     },
     cardContainer: {
         maxWidth: 345,
         margin: "5rem auto"
-    }
+    },
+    particlesCanva: {
+        position: "absolute",
+        opacity: "0.3",
+    },
 })
 
 const Portfolio = () => {
@@ -42,11 +49,55 @@ const Portfolio = () => {
     function handleShow3() {
         prj3Show ? setprj3Show(false) : setprj3Show(true);
     }
+    const [prj4Show, setprj4Show] = useState(false);
+    function handleShow4() {
+        prj4Show ? setprj4Show(false) : setprj4Show(true);
+    }
 
     const classes = useStyles();
     return (
         <Box component="div" className={classes.mainContainer}>
             <Navbar />
+            <Particles canvasClassName={classes.particlesCanva}
+                params={{
+                    particles:{
+                        number: {
+                            value: 45,
+                            density: {
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape: {
+                            type: "circle",
+                            strole: {
+                                width: 1,
+                                color: "tomato"
+                            }
+                        },
+                        size: {
+                            value: 8,
+                            random: true,
+                            animation: {
+                                enable: true,
+                                speed: 10,
+                                size_min: 0.1,
+                                sync: true
+                            }
+                        },
+                        opacity: {
+                            value: 1,
+                            random: true,
+                            animation: {
+                                enable: true,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: true
+                            }
+                        }
+                    }
+                }}
+            />
             <Grid container justify="center" alignItems="center">
             {/* Project 1 */}
                 <Grid item xs={12} sm={8} md={6}>
@@ -139,7 +190,39 @@ const Portfolio = () => {
                                 <Button size="small" color="primary" href="https://youtu.be/Qd2JnjiEkVg">
                                     Youtube
                                 </Button>
-                                <Button size="small" color="primary" href="/portfolio/welcometothefuture">
+                                <Button size="small" color="primary" href="https://github.com/FOXPP2047/weclometothefuture_what_I_did">
+                                    Github
+                                </Button>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                {/* Project 4 discord riot api */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia 
+                            component="img" 
+                            alt="Project 4"
+                            height="140" 
+                            image={project4} 
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Discord League of Legend Record Searcher
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    { prj4Show ? riotExplanation : riotExplanation.substr(0, 100) + "..." }
+                                    <Button component="span" size="small" color="primary" onClick={handleShow4}>
+                                        { prj4Show ? "less" : "more" }
+                                    </Button>
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary" href="">
+                                    Youtube(Not Yet)
+                                </Button>
+                                <Button size="small" color="primary" href="https://github.com/FOXPP2047/riot_games_discord_bot">
                                     Github
                                 </Button>
                             </CardActions>
